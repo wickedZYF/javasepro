bf0f7edada95a64b1a9116b54b9203439ad14b7d
wickedZYF <1002835548@qq.com>
2022-02-23 18:10:14 +0800

    重学java，day1

---
+++ hello-app/src/com/zyf/literal/LiteralDemo.java
@@ -0,0 +1,30 @@
+package com.zyf.literal;
+
+public class LiteralDemo {
+    public static void main(String[] args) {
+        //书记的书写格式
+        //1.整数
+        System.out.println(666);
+        //2.小数
+        System.out.println(99.5);
+        //3.字符
+        System.out.println('a');
+        System.out.println('0');
+        System.out.println('中');
+        System.out.println("中国");
+        System.out.println(' ');//空字符
+        //特殊字符：\n 代表是换行 \t 代表tab
+        System.out.println('中');
+        System.out.println('\n');
+        System.out.println('国');
+        System.out.println('\t');
+        //4.字符串，用双引号围起来，内容可以随意
+        System.out.println("我爱你中国");
+        System.out.println("");
+        System.out.println(" ");
+        System.out.println("我");
+        //5.布尔值 ，true false
+        System.out.println(true);
+        System.out.println(false);
+    }
+}
---
+++ hello-app/src/com/zyf/note/NoteDemo.java
@@ -0,0 +1,19 @@
+package com.zyf.note;
+
+public class NoteDemo {
+    public static void main(String[] args) {
+        /**
+         目标，学会使用注释
+         这个程序也是讲解注释的
+         */
+
+        //瞎写一桶
+        System.out.println("我要开始学习java程序");
+        /*
+       床前明月光
+       疑是地上霜
+       举头望明月
+       低头思故乡
+         */
+    }
+}
